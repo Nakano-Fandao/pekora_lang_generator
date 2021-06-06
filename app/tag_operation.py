3047cb15ac8b298798d6c42f6f35c9e6af7d12b3
kazuki-nishimura <71303399+kazuki-nishimura@users.noreply.github.com>
2021-06-06 16:59:49 +0900

    Add operations on HTML tags

---
+++ app/tag_operation.py
@@ -0,0 +1,36 @@
+import re
+
+def replace_tags(sentence):
+
+    # Create pattern list
+    tag_list = [
+        '<a.*?>',
+        "</a>",
+        '<s.*?>',
+        "</s>"
+    ]
+
+    # Tag pattern
+    patterns = re.compile(('|').join(tag_list))
+
+    # Matched tags
+    tags = patterns.findall(sentence)
+
+    # Replacing process
+    replaced_sentence = re.sub(('|').join(tags), "TAG_FLAG", sentence)
+
+    return replaced_sentence, tags
+
+def return_tags(sentence, tags):
+
+    for tag in tags:
+        sentence = sentence.replace("TAG_FLAG", tag, 1)
+
+    return sentence
+
+
+sentence = """
+"""
+
+if __name__ == "__main__":
+    replace_tags(sentence)
