00d28e330f2c265b31c58e5f6736cd2c1f43719f
kazuki-nishimura <71303399+kazuki-nishimura@users.noreply.github.com>
2021-06-20 21:01:36 +0900

    Update name

--- contents/modules/switch.js
+++ contents/modules/switch.js
@@ -1,14 +1,39 @@
-const switch_imgs = () => {
+// Variables
+const ad_kwd_list = ["googleads", "aswift", "banner", "adsystem", "ad"];
+const iframe_tags = document.getElementsByTagName('iframe');
+
+
+
+const switchImgs = () => {
+
+    switchAdFrame();
+
+
     console.log("Switching images completed")
     import("./loading.js").then( module => module.removeLoading() );
     import("./almond_status.js")
-        .then( module => module.updateAlmondStatus("images", true) );
+        .then( module => module.updateAlmondStatus("image", true) );
 }
 
 const returnOriginalImgs = () => {
     import("./almond_status.js")
-        .then( module => module.updateAlmondStatus("images", false) );
+        .then( module => module.updateAlmondStatus("image", false) );
 
 }
 
-export {switch_imgs, returnOriginalImgs};
+const switchAdFrame = () => {
+    const ad_frame = Array.from(iframe_tags)
+        .filter( item => tagFilter(item, ad_kwd_list) );
+}
+
+const tagFilter = (item, kwd_list) => {
+    const id_bool = kwd_list.map( kwd => item.id.includes(kwd) ).includes(true);
+    const class_bool = kwd_list.map( kwd => item.class.includes(kwd) ).includes(true);
+    const src_bool = kwd_list.map( kwd => item.src.includes(kwd) ).includes(true);
+
+    return [id_bool, class_bool, src_bool].includes(true);
+}
+
+
+
+export {switchImgs, returnOriginalImgs};
